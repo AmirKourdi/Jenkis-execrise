ffabf7a70b717efb12f81c7737b9909079d20017
AmirKourdi <amir_kourdi@hotmail.com>
2022-01-31 01:33:36 +0200

    Update app.py

--- simple-python-webapp-flask/app.py
+++ simple-python-webapp-flask/app.py
@@ -1,14 +1,16 @@
 import os
 from flask import Flask
+import socket
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
+@app.route('/DOCKER')
 def hello():
-    return 'I am good, how about you?'
+    docker_short_id = str(socket.gethostname())
+    return docker_short_id
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
